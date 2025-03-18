20a9be73476234c5d3f865067fbe68485460501f
Yuliya Karimova <y.o.karimova@gmail.com>
2025-03-18 18:15:03 +0300

    build: add test db

--- server/src/app.js
+++ server/src/app.js
@@ -7,13 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Подключение к MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/subscriptions', {
+// Определяем имя базы: если тесты, то используем "subscriptions_test", иначе "subscriptions"
+const dbName = process.env.NODE_ENV === 'test' ? 'subscriptions_test' : 'subscriptions';
+
+mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).then(() => {
+  console.log(`Connected to database: ${dbName}`);
+}).catch(err => {
+  console.error('Database connection error:', err);
 });
 
 // Маршруты
 app.use('/api/subscriptions', subscriptionRoutes);
 
-module.exports = app; // Теперь тесты могут использовать этот файл
+module.exports = app;
